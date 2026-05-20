a663cb85a94aede36290aed427826c05c39f7922
valentinaromar1 <200642560@student.georgianc.on.ca>
2026-05-20 11:35:42 -0400

    finnished lession 3

--- lesson-3/js/products.js
+++ lesson-3/js/products.js
@@ -1,26 +1,48 @@
 // STEP 1: Declare and initialize variables
 // STEP 1a: Grab the parts of the DOM that we need to build the invoice
-
+const productList = document.querySelector("tbody");
+const totalData = document.querySelector("tfoot td:frist-of-type")
 // STEP 1b: Build the products array in the format 'Product Name:0.00'
-
+let product = [
+	"frozen pizza: 5.99", 
+	"orange juice: 4.99", 
+	"milk: 5.95", 
+	"eggs: 5.99", 
+	"ham: 7.50", 
+	"cheese: 3:50"];
 // STEP 1c: Set up invoiceTotal variable - start at zero
+let invoiceTotal = 0;
 
 // STEP 1d: Declare the itemRow and the itemDetail array;
+let itemRow = new Array();
+let itemDeltail = new Array();
+let itemDesc;
+let itemPrice;
+let counter = 0;
 
 // STEP 2: Build a loop to iterate through the products array
-
+product.forEach((product) =>{
 	// STEP 3: Break apart the product name from the price for each item with split()
-
+	product = product.split(": ")
 	// STEP 4: Now we have an array as an element of an array - set the second array element to the product price (as type number)
-
+	product[1] = Number(product[1]);
+	console.log(product[1]);
 	// STEP 5: Add the price of this product to the invoice total
+	invoiceTotal += product[1];
 
 	// STEP 6: Capture each product name and price as variables 
-
+	itemDesc = product[0];
+	itemPrice = product[1];
 	// STEP 7: Create a TR element for this product and price in the invoice table
-
+	itemRow[counter] = document.createElement("tr");
 	// STEP 8: Build the string that contains two TD elements each containing one of the item description, and the item price
-
+	itemDeltail[counter] = `<td>${itemDesc}</td><td>${itemPrice}</td>`
 	// STEP 9: Set the above string as the innerHTML of the new TR element, and then append the new element to the table body (var productList)
-
+	itemRow[counter].innerHTML = itemDeltail[counter];
+	
+	productList.append(itemRow[counter]);
+	counter ++;
+}
+);
 // STEP 10: Set the total cost of the invoice as the textContent of the TD in the TFOOT (var totalData), rounding the number to two decimal places
+totalData.textContent = "$" + invoiceTotal;
