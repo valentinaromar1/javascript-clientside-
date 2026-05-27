f8dc1918cb47f2af8bbcc62d07a058386ceced59
valentinaromar1 <200642560@student.georgianc.on.ca>
2026-05-27 10:52:13 -0400

    done week 4

--- lesson-4/js/conditionals.js
+++ lesson-4/js/conditionals.js
@@ -3,6 +3,15 @@ const output = document.querySelector('body p:nth-of-type(2)');
 /* STEP 1a: A simple if/else statement */
 let homeWorkDone = null;
 
+let breakDuration;
+
+if (homeWorkDone) {
+	breakDuration = 60;
+} else{
+	breakDuration = 10;
+}
+
+output.textContent = `i get ${breakDuration} minutes for a break`;
 // STEP 1b: Any value that is not false, undefined, null, 0, NaN, or an empty string will evaluate to TRUE when tested using a conditional statement - try a few other values for var homeWorkDone and test
 
 /* STEP 2: The more complex if|elseif|else */
@@ -14,34 +23,54 @@ const weatherButton = document.querySelector('#weatherButton');
 const comments = document.querySelector('#commentary');
 
 /* STEP 2a: Add an event listener for the 'click' event on the 'Set Weather' button that invokes the setWeather() function below */
-
+weatherButton.addEventListener("click", setWeather);
 
 function setWeather() {
+	// console.log("EventListener works");
 	let choice = select.value;
 	let temperature = temp.value;
+	console.log(choice);
 	/* STEP 2b: Craft an IF/ELSEIF/ELSE that changes the src attribute of the icon element to the appropriate .svg file in the images folder */
-
+	if (choice === "sunny") {
+		//icon.setAttribute("src", "images/sunny.svg");
+		icon.src = "image/sunny.svg";
+		
+	} else if (choice === "rainy"){
+		icon.src = "images/rainy.svg";
+	}else if (choice === "windy"){
+		icon.src = "images/windy.svg";
+	} else {
+		icon.src = "images/cloud-off.svg";
+	}
+	setBackgroundColor(temperature);
 	/* STEP 3: Add a nested IF/ELSE statement inside the 'sunny' condition above that tests for temperature, and if it is equal to or greater than 15, turn the page background orange, otherwise turn it lightblue */
-
+	function setBackgroundColor(temperature){
+		//change screen color based on temp
+		if(temperature >= 15){
+			page.style.backgroundColor = "lightorange";
+		}else{
+			page.style.backgroundColor = "lightblue";
+		}
+	}
 	/* STEP 4: Logical operators - and, or, not (delete multi-line comment delimiters below) */
 	// STEP 4a: AND - && (It's sunny AND it's at least 15deg)
-/*
-	if () {
+
+	if (choice === "sunny" && temperature >= 15) {
 		comments.textContent = 'It is sunny and warm — time to find your sunglasses!';
 	// STEP 4b: AND, once again - && (It's sunny AND it's less than 15deg)
-	} else if () {
+	} else if (choice === "sunny" && temperature <15) {
 		comments.textContent = 'It is sunny out there, but it is a bit cool.';
 	// STEP 4c: OR - || (It's windy OR rainy)
-	} else if () {
+	} else if (choice === "windy" || choice === "rainy") {
 		comments.textContent = 'The weather today is a bit unsettled.';
 	} else {
 		comments.textContent = 'You did not specify the type of weather, today.';
 	}
 	// STEP 4d: NOT - ! (It's NOT rainy)
-	if () {
+	if (choice != "rainy") {
 		comments.textContent += ' No sign of rain.';
 	}
-*/
+
 	// Weather icons by Cole Bemis - https://feathericons.com/, MIT, https://commons.wikimedia.org/w/index.php?curid=60153354
 
 } // End of setWeather() function
@@ -55,23 +84,32 @@ const windComment = document.querySelector('#windComment');
 windSpeed.addEventListener('change', getWindSpeed);
 
 function getWindSpeed() {
-let speed = Number(windSpeed.value);
+	let speed = Number(windSpeed.value);
 // STEP 5a: Build the SWITCH code block
-
+	switch(speed){
 	// STEP 5b: Build out four cases, followed by a default
-
-		// 100 km/h
-		windComment.textContent = speed + 'km/h - Holy schmoly! Hold on to your hat!';
-
-		// 75 km/h
-		windComment.textContent = speed + 'km/h - It\'s a-howling out there!';
-
-		// 50 km/h
-		windComment.textContent = speed + 'km/h - Quite gusty out there, today.';
-
-		// 25 km/h
-		windComment.textContent = speed + 'km/h - A wee bit breezy.';
-
+			case 100:
+				// 100 km/h
+				windComment.textContent = speed + 'km/h - Holy schmoly! Hold on to your hat!';
+				break;
+			case 75:
+			// 75 km/h
+				windComment.textContent = speed + 'km/h - It\'s a-howling out there!';
+				break;
+			case 50:
+				// 50 km/h
+				windComment.textContent = speed + 'km/h - Quite gusty out there, today.';
+				
+			case 25:
+				// 25 km/h
+				windComment.textContent = speed + 'km/h - A wee bit breezy.';
+				break;
+			case 0:
+				windComment.textContent = speed + 'Km/h - no wind at all';
+				break;
+			default:
+				windComment.textContent = "no wind speed available"
+		}
 } // End of getWindSpeed() function
 
 
@@ -80,5 +118,5 @@ This piece of syntax is a bit less typing, but it is harder to read */
 let breakMessage = document.querySelector('#breakTime');
 let coffeeBreak = true;
 
-
+coffeeBreak === true ? breakMessage = "coffee time!" : breakMessage = "keep working";
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals
--- lesson-4/js/loops.js
+++ lesson-4/js/loops.js
@@ -3,6 +3,13 @@ let courseList = 'My courses this term are ';
 const msg = document.querySelector('#courses');
 /* STEP 1a: A simple FOR loop
 A simple loop of this structure needs an initializer (counter variable), an exit conditional, and and incrementor/decrementor */
+for (let i = 0; i < courses.length; i++){
+	if (i === courses.length - 1){
+		courseList += `${courses[i]}.`;
+	}else{
+		courseList += `${courses[i]},`;
+	}
+}
 
 msg.textContent = courseList;
 // STEP 1b: Add a conditional for the last item in the array so we can finish the sentence with a period.
@@ -14,13 +21,20 @@ const input = document.querySelector('input');
 const btn = document.querySelector('button#cities');
 
 btn.addEventListener('click', function() {
-	var searchName = input.value;
+	let searchName = input.value;
 	input.value = '';
 	input.focus();
 	/* STEP 2a: Create a FOR loop to iterate through the cities array */
-
+	for (let i = 0; i < cities.length; i++){
 		/* STEP 2b: Build an IF/ELSE conditional that compares searchName with cities[i] */
-
+		if (searchName.toLowerCase() === cities[i].toLowerCase){
+			//step 2 c
+			result.textContent = `found ${cities}`;
+			break;
+		}else{
+			result.textContent = `sorry, ${searchName} not found`;
+		}
+	}
 });
 
 
@@ -31,10 +45,10 @@ let num = 50;
 for (let i = 1; i <= num; i ++) {
 	let sqRoot = Math.sqrt(i);
 	/* STEP 3a: Build an IF statement that checks whether the square root of the number is NOT an integer */
-	//if () {
-		/* STEP 3b: If the square root of the number is not an integer, we don't want to output it to the paragraph, so skip the rest of the instructions inside this loop and go back up to the FOR, using 'continue' */
-
-	//}
+	if (Math.floor(sqRoot) !== sqRoot) {
+		/* STEP 3b: If the square root of the number is not an integer, we don't want to output it to the paragraph, so skip the rest of the instructions inside this loop and go back up to the FOR, using continue' */
+		continue;
+	}
 	numberList.textContent += i + ' ';
 }
 
@@ -43,18 +57,26 @@ for (let i = 1; i <= num; i ++) {
 let j = 0;
 let outputWhile = '';
 /* STEP 4a: WHILE loop to iterate through the cities array (created above in STEP 2) */
+while(j < cities.length){
+	outputWhile += cities[j] + "/";
+	j ++;
 
+}
 console.log(outputWhile);
 
 let k = 0;
 var outputDo = '';
 /* STEP 4b: DO/WHILE loop - grab the above IF/ELSE and the 'i++', then create the very same output with DO/WHILE (uncomment the above 'i = 0', first) */
-
+do{
+	outputDo += cities[k] + "/"
+}while(k >= cities.length - 1);
 console.log(outputDo);
 /* …now try to loop through the same array backwards! */
 
 /* STEP 5: The elegant FOREACH loop (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach) */
-
+cities.forEach((city) =>{
+	console.warn(city);
+})
 
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code
